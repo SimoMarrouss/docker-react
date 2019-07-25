a99efb48b4e2df0dd3d60011e6e5510860ebdc45
Mohammed Marrouss <mmarrouss@sqli.com>
2019-07-25 11:16:59 +0100

    add new message

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello worll
+          Fuck you ismail Oukassou
         </a>
       </header>
     </div>
